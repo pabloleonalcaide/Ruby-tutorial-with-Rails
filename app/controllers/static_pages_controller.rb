27d481a1acdfdb1a4e2d2c96beab6086038796c7
pabloleonalcaide <pabloleonpsico@gmail.com>
2019-07-16 21:28:59 +0200

    playing with static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
+  # Rails provide the connection between controller action and view
   def home
   end
 
   def help
+  end
+
+  def about
   end
 end
