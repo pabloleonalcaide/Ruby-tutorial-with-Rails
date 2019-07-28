6eb9982a098c935bed26cc7702b00ddf5d2e2b92
pabloleonalcaide <pabloleonpsico@gmail.com>
2019-07-28 17:40:53 +0200

    Finish login/logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user  # sessions_helper#log_in
+      # if clicked remember_me checkbox
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
 
+    attr_accessor :remember_token
+
     before_save { self.email = email.downcase }
+    
+    #validations
     valid_mail_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name,  presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, 
         format: {with: valid_mail_regex}, 
         uniqueness: { case_sensitive: false }
-    
     validates :password, presence: true, length: { minimum: 6 }
     has_secure_password  # to works, it requires an attribure 'pasword_digest' in the model
 
@@ -15,5 +18,26 @@ class User < ActiveRecord::Base
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token using lib SecureRandom
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Remembers a user in the database for use in persistent sessions.
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Forgets a user.
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+    # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 end
